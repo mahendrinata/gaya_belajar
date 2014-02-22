f273be3ddb163073f6715d06ed42e8e2aa7def99
Mahendri Winata <mahen.0112@gmail.com>
2014-02-22 17:16:52 +0700

    change title

--- application/views/layout/blank.php
+++ application/views/layout/blank.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Gaya Belajar</title>
+    <title>LBB GenPI Education Merauke</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--[if lt IE 9]>
     <?php echo js(array('html5')); ?>
