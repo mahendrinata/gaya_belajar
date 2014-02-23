7dc33bbb016ebfd497db5b9452d237e3c73470c4
Mahendri Winata <mahen.0112@gmail.com>
2014-02-23 19:14:41 +0700

    add function for edit account

--- application/views/content/admin/pengguna/edit_account.php
+++ application/views/content/admin/pengguna/edit_account.php
@@ -3,9 +3,12 @@
 </div>
 <?php
 echo form_open('admin/pengguna/edit_account/', array('class' => 'form-horizontal'));
+if ($pengguna['level'] != Level::ADMIN) {
+  echo bootstrap_form_input('username', $pengguna['username'], array('class' => 'span6', 'placeholder' => 'Username', 'label' => 'Username' . bootstrap_text_important()));
+}
 echo bootstrap_form_input('nama', $pengguna['nama'], array('class' => 'span6', 'placeholder' => 'Nama', 'label' => 'Nama' . bootstrap_text_important()));
 echo bootstrap_form_input('tempat_lahir', $pengguna['tempat_lahir'], array('class' => 'span6', 'placeholder' => 'Tempat Lahir', 'label' => 'Tempat Lahir'));
-echo bootstrap_form_input('tanggal_lahir', $pengguna['tanggal_lahir'], array('readonly' =>'readonly', 'class' => 'datepicker span2', 'placeholder' => 'Tempat Lahir', 'label' => 'Tanggal Lahir'));
+echo bootstrap_form_input('tanggal_lahir', $pengguna['tanggal_lahir'], array('readonly' => 'readonly', 'class' => 'datepicker span2', 'placeholder' => 'Tempat Lahir', 'label' => 'Tanggal Lahir'));
 echo bootstrap_form_dropdown('jenis_kelamin', array($pengguna['jenis_kelamin']), array('class' => 'span2', 'list' => array('Laki - Laki' => 'Laki - Laki', 'Perempuan' => 'Perempuan'), 'label' => 'Jenis Kelamin'));
 echo bootstrap_form_textarea('alamat', $pengguna['alamat'], array('class' => 'span8', 'placeholder' => 'Alamat', 'rows' => 6, 'label' => 'Alamat'));
 echo bootstrap_form_input('agama', $pengguna['agama'], array('class' => 'span6', 'placeholder' => 'Agama', 'label' => 'Agama'));
